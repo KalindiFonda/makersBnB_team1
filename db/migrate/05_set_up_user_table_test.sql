afadd0d8e7f29c27e31010e8a4c51da86c182a63
Lorna Roberts <lornaroberts99@gmail.com>
2020-11-11 14:20:03 +0000

    apostrophe error fixed

---
+++ db/migrate/05_set_up_user_table_test.sql
@@ -0,0 +1,14 @@
+\c cat_manager_test;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  email VARCHAR(60),
+  password VARCHAR(140)
+                );
+
+
+INSERT INTO users (name, email, password)
+  VALUES ('Peter',
+          'test@test.com',
+          'password'
+           );
